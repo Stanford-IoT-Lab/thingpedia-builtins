4e34bc901eec115f5de4fc124bb366ac9ec3c4ec
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-11 12:15:29 -0800

    Import from thingengine-core

---
+++ omlet/incomingmessage.js
@@ -0,0 +1,17 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2015 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//                Jiwon Seo <jiwon@cs.stanford.edu>
+//
+// See COPYING for details
+
+const Tp = require('thingpedia');
+const InMessageChannel = require('./inmessage');
+
+module.exports = new Tp.ChannelClass({
+    Name: 'IncomingMessageChannel',
+    Extends: InMessageChannel,
+    signal: 'incoming-message',
+});
